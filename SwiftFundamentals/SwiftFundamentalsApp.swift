644fe984017d30f17c41310d47d779b44b29f3fb
RyotaFukumoto <f.ryota.828@gmail.com>
2024-01-08 10:10:59 +0900

    Initial Commit

---
+++ SwiftFundamentals/SwiftFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFundamentalsApp.swift
+//  SwiftFundamentals
+//
+//  Created by ryota on 2024/01/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
